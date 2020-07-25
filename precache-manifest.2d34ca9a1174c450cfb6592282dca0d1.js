46c991881fc309ff255025409df04e30cdc01000
sumit <sharmasumit0308@gmail.com>
2020-07-25 17:11:39 +0530

    Updates

---
+++ precache-manifest.2d34ca9a1174c450cfb6592282dca0d1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afb46c3eec8ccf77c2e76790bdb1f857",
+    "url": "/Nikhil_Website/index.html"
+  },
+  {
+    "revision": "3615f0f2466a97a3087e",
+    "url": "/Nikhil_Website/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c8a3c166e61cc0e5b503",
+    "url": "/Nikhil_Website/static/css/main.afde4826.chunk.css"
+  },
+  {
+    "revision": "3615f0f2466a97a3087e",
+    "url": "/Nikhil_Website/static/js/2.d297bdb8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Nikhil_Website/static/js/2.d297bdb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8a3c166e61cc0e5b503",
+    "url": "/Nikhil_Website/static/js/main.3b0100a3.chunk.js"
+  },
+  {
+    "revision": "9f67a07ca7878290a4b8",
+    "url": "/Nikhil_Website/static/js/runtime-main.2483397d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Nikhil_Website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "67184173b0af641a747b78eeb5772372",
+    "url": "/Nikhil_Website/static/media/pizza.67184173.jpg"
+  }
+]);
